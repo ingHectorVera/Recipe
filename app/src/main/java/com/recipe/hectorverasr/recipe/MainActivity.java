8f3ec3f8ff52072ab5d81790e7c7c2565a7496b7
ingHectorVera <verahector26@gmail.com>
2017-09-06 18:20:14 -0400

    changes

--- app/src/main/java/com/recipe/hectorverasr/recipe/MainActivity.java
+++ app/src/main/java/com/recipe/hectorverasr/recipe/MainActivity.java
@@ -3,7 +3,12 @@ package com.recipe.hectorverasr.recipe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
